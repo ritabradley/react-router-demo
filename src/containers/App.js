14cc439f04a83eba97e608a4fd6f3518432d7716
Rita Bradley <contact@ritabradley.com>
2020-12-17 18:12:55 -0800

    Remove CRA boilerplate and configure Tailwind CSS

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hi world!</h1>
+        </div>
+    );
+};
+
+export default App;
