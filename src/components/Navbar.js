23c22ad7349f7b3fdb460396af4574ef4eb9bf40
Rita Bradley <contact@ritabradley.com>
2020-12-17 18:53:59 -0800

    Style and add interactvity to navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    const [navbarOpen, setNavbarOpen] = React.useState(false);
+    return (
+        <div>
+            <nav className='relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-gray-700 mb-3'>
+                <div className='container px-4 mx-auto flex flex-wrap items-center justify-between'>
+                    <div className='w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start'>
+                        <Link
+                            to='/'
+                            className='text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white'
+                        >
+                            Router Demo
+                        </Link>
+                        <button
+                            className='cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none'
+                            type='button'
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <span className='block relative w-6 h-px rounded-sm bg-white'></span>
+                            <span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
+                            <span className='block relative w-6 h-px rounded-sm bg-white mt-1'></span>
+                        </button>
+                    </div>
+                    <div
+                        className={'lg:flex flex-grow items-center' + (navbarOpen ? ' flex' : ' hidden')}
+                        id='example-navbar-warning'
+                    >
+                        <ul className='flex flex-col lg:flex-row list-none ml-auto'>
+                            <li className='nav-item'>
+                                <Link
+                                    to='/'
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                >
+                                    Home
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    to='/category'
+                                >
+                                    Category
+                                </Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link
+                                    className='px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75'
+                                    to='/products'
+                                >
+                                    Products
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 import { Link, Route, Switch } from 'react-router-dom';
 
 const App = () => {
     return (
         <div>
-            <h1>Hi world!</h1>
+            <Navbar />
         </div>
     );
 };
